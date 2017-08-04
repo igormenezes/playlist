7463a9baa75328057dd48553e57a1265a5dec08b
Igor Menezes <imigormenezes@gmail.com>
2017-08-04 18:30:36 -0300

    UsersTest - Unit Test com a classe Users

--- tests/Unit/UsersTest.php
+++ tests/Unit/UsersTest.php
@@ -5,27 +5,98 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\DB;
 
-use App\Http\Controllers\UsersController;
+use App\Model\Users;
 
-class UsersTest extends TestCase{
+class UsersTest extends TestCase
+{
+	public function setUp()
+	{
+		parent::setUp();
+		DB::beginTransaction();
+	}
 
-	public function testIndex(){
-		$response = $this->withSession(['administrator' => 1, 'login' => true])->get('/');
+	public function testAddAdminUser()
+	{
+		$response = $this->json('POST', '/create', array(
+			'name' => 'Teste Admin',
+			'password' => '123',
+			'email' => 'testeadmin@yahoo.com.br',
+			'administrator' => 1,
+			'age' => 25
+			));
+
+		$response->assertStatus(302);
+	}
+
+	public function testAddComumUser()
+	{
+		$response = $this->json('POST', '/create', array(
+			'name' => 'Teste',
+			'password' => '123',
+			'email' => 'teste@yahoo.com.br',
+			'age' => 25
+			));
+
+		$response->assertStatus(302);
+	}
+
+	public function testAddRepeatUser()
+	{
+		$users = new Users;
+		$users->name = 'Teste';
+		$users->password = encrypt('123');
+		$users->email = 'teste@yahoo.com.br';
+		$users->age = 23;
+		$users->save();
+
+		$response = $this->json('POST', '/create', array(
+			'name' => 'Teste',
+			'password' => '123',
+			'email' => 'teste@yahoo.com.br',
+			'age' => 25
+			));
+
+		$response->assertViewIs('register');
+	}
+
+	public function testLoginAdminUser()
+	{
+		$users = new Users;
+		$users->name = 'Teste';
+		$users->password = encrypt('123');
+		$users->email = 'testeadmin@yahoo.com.br';
+		$users->age = 23;
+		$users->administrator = 1;
+		$users->save();
+
+		$response = $this->json('POST', '/login', array(
+			'email' => 'testeadmin@yahoo.com.br',
+			'password' => '123'
+			));
+
 		$response->assertRedirect('/music');
+	}
 
-		$response = $this->withSession(['administrator' => 0, 'login' => true])->get('/');
+	public function testLoginComumUser()
+	{
+		$users = new Users;
+		$users->name = 'Teste';
+		$users->password = encrypt('123');
+		$users->email = 'teste@yahoo.com.br';
+		$users->age = 23;
+		$users->save();
+
+		$response = $this->json('POST', '/login', array(
+			'email' => 'teste@yahoo.com.br',
+			'password' => '123'
+			));
+
 		$response->assertRedirect('/list');
-
-		$this->get('/')->assertViewIs('index');
 	}
 
-	public function testLogin(){
-		$this->assertTrue(true);
-
-	}
-
-	public function testCreate(){
-		$this->assertTrue(true);
+	public function tearDown(){
+		DB::rollback();
 	}
 }
